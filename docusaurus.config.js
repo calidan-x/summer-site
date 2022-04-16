05c37d6d6bef9b091ebc41bf70780cdcdff5a73d
Calidan <calidan@vip.qq.com>
2022-04-16 14:12:20 +0800

    fix: update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,36 +92,6 @@ const config = {
                 to: '/docs/intro'
               }
             ]
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus'
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus'
-              }
-            ]
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog'
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus'
-              }
-            ]
           }
         ],
         copyright: `Copyright © ${new Date().getFullYear()} SummerJS.`
