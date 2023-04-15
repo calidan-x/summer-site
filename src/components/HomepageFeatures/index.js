6157d1da9a383fb9f880ce64b93ec11818328b1a
Calidan <calidan@vip.qq.com>
2023-04-15 22:11:42 +0800

    update docs

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,28 +1,64 @@
-import React from 'react'
-import clsx from 'clsx'
-import styles from './styles.module.css'
+import React from "react"
+import clsx from "clsx"
+import styles from "./styles.module.css"
 
-const FeatureList = [
-  {
-    title: 'Programmer Friendly',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: <>every things just as simple & powerful as possible</>
-  },
-  {
-    title: 'Runtime TypeScript',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>strict runtime typing validation and swagger typing document</>
-  },
-  {
-    title: 'More',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: <>customer decorators, easy DTO conversion, serverless compatible build, RPC …</>
-  }
-]
+const isChinese = location.href.includes("zh-CN")
+
+const FeatureList = isChinese
+  ? [
+      {
+        title: "程序员友好",
+        Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+        description: <>学习成本低，功能强大，简单易用</>
+      },
+      {
+        title: "运行时 TypeScript",
+        Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+        description: (
+          <>严格的运行时 TypeScript 验证，自动生成 Swagger 类型文档</>
+        )
+      },
+      {
+        title: "其他",
+        Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+        description: (
+          <>
+            自定义装饰器，丰富的扩展（MySQL, Redis,
+            GraphQL..），代码打包（支持Serverless模式），RPC调用...
+          </>
+        )
+      }
+    ]
+  : [
+      {
+        title: "Programmer Friendly",
+        Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+        description: <>every things just as simple & powerful as possible</>
+      },
+      {
+        title: "Runtime TypeScript",
+        Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+        description: (
+          <>strict runtime typing validation and swagger typing document</>
+        )
+      },
+      {
+        title: "More",
+        Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+        description: (
+          <>
+            customer decorators, rich extension(MySQL, Redis, GraphQL..),
+            serverless compatible build, RPC …
+          </>
+        )
+      }
+    ]
+
+// translate({ id: "homepage.TagLine" })
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import clsx from 'clsx'
-import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import styles from './index.module.css'
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import React from "react"
+import clsx from "clsx"
+import Layout from "@theme/Layout"
+import Link from "@docusaurus/Link"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import styles from "./index.module.css"
+import HomepageFeatures from "@site/src/components/HomepageFeatures"
 
-import Translate, { translate } from '@docusaurus/Translate'
+import Translate, { translate } from "@docusaurus/Translate"
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx('hero', styles.heroBanner)} style={{ backgroundColor: 'var(--ifm-color-banner-bg)' }}>
+    <header
+      className={clsx("hero", styles.heroBanner)}
+      style={{ backgroundColor: "var(--ifm-color-banner-bg)" }}
+    >
       <div className="container">
         <img width="100px" src="/img/logo.svg" />
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+          <Translate id="homepage.TagLine">
+            Simple / Powerful / Enterprise
+          </Translate>
+        </p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" style={{ borderRadius: '25px' }} to="/docs/intro">
-            Getting Start
+          <Link
+            className="button button--secondary button--lg"
+            style={{ borderRadius: "25px" }}
+            to="/docs/intro"
+          >
+            <Translate id="homepage.GettingStart">Get Started</Translate>
           </Link>
         </div>
       </div>
@@ -39,8 +50,11 @@ export default function Home() {
   )
 }
 
-if (typeof window !== 'undefined') {
-  if (window.location.href.indexOf('www.summerjs.dev') > 0) {
-    window.location.href = window.location.href.replace('www.summerjs.dev', 'summerjs.dev')
+if (typeof window !== "undefined") {
+  if (window.location.href.indexOf("www.summerjs.dev") > 0) {
+    window.location.href = window.location.href.replace(
+      "www.summerjs.dev",
+      "summerjs.dev"
+    )
   }
 }
